8e3ae2a2a1c8aa6efdf301e62ea57a3ef7737fb7
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2021-10-12 21:46:06 +0200

    adde twodimensional ca, life-like constructor

--- src/CellularAutomata.jl
+++ src/CellularAutomata.jl
@@ -2,6 +2,7 @@ module CellularAutomata
 
 abstract type AbstractRule end
 abstract type AbstractODRule <: AbstractRule end
+abstract type AbstractTDRule <: AbstractRule end
 abstract type AbstractCA end
 
 struct CellularAutomaton{F,E} <: AbstractCA
@@ -23,6 +24,19 @@ function CellularAutomaton(rule::AbstractODRule, initial_conditions, generations
 
 end
 
+function CellularAutomaton(rule::AbstractTDRule, initial_conditions, generations)
+
+    evolution = zeros(typeof(initial_conditions[2]), size(initial_conditions, 1), size(initial_conditions, 2), generations)
+    evolution[:, :, 1] = initial_conditions
+
+    for i=2:generations
+        evolution[:, :, i] = rule(evolution[:, :, i-1])
+    end
+
+    CellularAutomaton(generations, rule, evolution)
+
+end
+
 export CellularAutomaton
 
 include("dca.jl")
@@ -33,5 +47,8 @@ export CCA
 
 include("tca.jl")
 export TCA
+
+include("life.jl")
+export Life
 
 end # module
---
+++ src/life.jl
@@ -0,0 +1,54 @@
+
+abstract type AbstractLifeRule <: AbstractTDRule end
+
+struct Life{T,A,C} <: AbstractLifeRule
+    born::T
+    survive::A
+    radius::C
+end
+
+function Life(life_description; radius=1)
+    born, survive = life_description[1], life_description[2]
+    Life(born, survive, radius)
+end
+
+function (life::Life)(starting_array)
+    return nextgen = life_evolution(starting_array, life.born, life.survive, life.radius)
+end
+
+function virtual_expansion(starting_array, radius)
+
+    height, width = size(starting_array)
+    nh, nw = height-radius+1, width-radius+1
+    left = vcat(starting_array[nh:end,nw:end], starting_array[:,nw:end], starting_array[1:radius, nw:end])
+    right = vcat(starting_array[nh:end, 1:radius], starting_array[:,1:radius], starting_array[1:radius,1:radius])
+    middle = vcat(starting_array[nh:end,:], starting_array, starting_array[1:radius,:])
+
+    hcat(left, middle, right)
+end
+
+function life_application(state, born, survive)
+
+    past_value = state[size(state, 1)÷2+1, size(state, 2)÷2+1]
+    alive = sum(state)-past_value
+
+    if past_value == 1 && alive in survive
+        return 1
+    elseif past_value == 0 && alive in born
+        return 1
+    else
+        return 0
+    end
+end
+
+function life_evolution(starting_array, born, survive, radius)
+
+    height, width = size(starting_array)
+    output = zeros(typeof(starting_array[2]), height, width)
+    virtual_output = virtual_expansion(starting_array, radius)
+
+    for i = 1:height-radius+1, j = 1:width-radius+1
+        output[i, j] = life_application(virtual_output[i:i+radius+1, j:j+radius+1], born, survive)
+    end
+    output
+end
