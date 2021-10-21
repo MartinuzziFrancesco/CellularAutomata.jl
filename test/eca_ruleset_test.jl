7f0544441beea22b74f102042ea92cfa537544a7
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2021-10-21 22:42:21 +0200

    fixed typo in tests

--- test/eca_ruleset_test.jl
+++ test/eca_ruleset_test.jl
@@ -6,10 +6,10 @@ const rule0 = DCA(0)
 const rule1 = DCA(1)
 @test rule1.ruleset == [1, 0, 0, 0, 0, 0, 0, 0] #http://atlas.wolfram.com/01/01/1/
 
-@const rule2 = DCA(2)
+const rule2 = DCA(2)
 @test rule2.ruleset == [0, 1, 0, 0, 0, 0, 0, 0] #http://atlas.wolfram.com/01/01/2/
 
-@const rule3 = DCA(3)
+const rule3 = DCA(3)
 @test rule3.ruleset == [1, 1, 0, 0, 0, 0, 0, 0] #http://atlas.wolfram.com/01/01/2/
 
 const rule30 = DCA(30)
