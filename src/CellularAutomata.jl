d8ca7ee29c1b10f3276eb16c5c777f9de08f1335
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2025-05-05 10:52:19 +0200

    refac: move rules to their own folder pt 2

--- src/CellularAutomata.jl
+++ src/CellularAutomata.jl
@@ -2,10 +2,10 @@ module CellularAutomata
 
 include("generics.jl")
 include("cellular_automaton.jl")
-include("dca.jl")
-include("cca.jl")
-include("tca.jl")
-include("life.jl")
+include("rules/dca.jl")
+include("rules/cca.jl")
+include("rules/tca.jl")
+include("rules/life.jl")
 include("measures.jl")
 
 export CellularAutomaton
